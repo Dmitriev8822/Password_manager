ab75dda42051ebd718436778e7f1d61015f58ca5
Lemon <79759760+Dmitriev8822@users.noreply.github.com>
2023-06-17 23:09:46 +0300

    0.9.1

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ from connect_db import Сonnect_DB_users, Сonnect_DB_data
 from pyperclip import copy
 
 
+import sys
+import platform
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import (QCoreApplication, QPropertyAnimation, QDate, QDateTime, QMetaObject, QObject, QPoint, QRect, QSize, QTime, QUrl, Qt, QEvent)
+from PyQt5.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont, QFontDatabase, QIcon, QKeySequence, QLinearGradient, QPalette, QPainter, QPixmap, QRadialGradient)
+from PyQt5.QtWidgets import *
+
+
 def except_hook(cls, exception, traceback):
     sys.__excepthook__(cls, exception, traceback)
+
+
+counter = 0
 
 
 class MainClass(QMainWindow):
@@ -67,6 +78,67 @@ class MainClass(QMainWindow):
             msg.setInformativeText(text)
             msg.setWindowTitle("Notice")
         msg.exec_()
+
+
+class SplashScreen(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('data/ui/splash_bar.ui', self)
+
+        ## UI ==> INTERFACE CODES
+        ########################################################################
+
+        ## REMOVE TITLE BAR
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+
+        ## DROP SHADOW EFFECT
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 60))
+        self.dropShadowFrame.setGraphicsEffect(self.shadow)
+
+        ## QTIMER ==> START
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.progress)
+        # TIMER IN MILLISECONDS
+        self.timer.start(35)
+
+        # CHANGE DESCRIPTION
+
+        # Initial Text
+        self.label_description.setText("<strong>WELCOME</strong> TO MY APPLICATION")
+
+        # Change Texts
+        QtCore.QTimer.singleShot(1500, lambda: self.label_description.setText("<strong>LOADING</strong> DATABASE"))
+        QtCore.QTimer.singleShot(3000, lambda: self.label_description.setText("<strong>LOADING</strong> USER INTERFACE"))
+
+    ## ==> APP FUNCTIONS
+    ########################################################################
+    def progress(self):
+
+        global counter
+
+        # SET VALUE TO PROGRESS BAR
+        self.progressBar.setValue(counter)
+
+        # CLOSE SPLASH SCREE AND OPEN APP
+        if counter > 100:
+            # STOP TIMER
+            self.timer.stop()
+
+            # SHOW MAIN WINDOW
+            win_sing_in.show()
+
+            # CLOSE SPLASH SCREEN
+            self.close()
+
+        # INCREASE COUNTER
+        counter += 1
+
 
 
 
@@ -190,10 +262,12 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
 
     mainWin = MainClass()
+    bar = SplashScreen()
     win_sing_in = WinSingIn()
     win_sing_up = WinSingUp()
 
-    win_sing_in.show()
+    # win_sing_in.show()
+    bar.show()
 
     sys.excepthook = except_hook
     sys.exit(app.exec_())
